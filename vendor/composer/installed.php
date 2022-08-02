913c15f4872d74b0bc6cd3e963a4b395e58534ff
Tanguy Dechiron <tanguy.dechiron@gmail.com>
2022-08-02 11:47:55 +0200

    autoloader + update blueprints

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'grav-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'dd2b29dea11c7251867110bd4ad99de5e185b5ae',
+        'name' => 'tanguy-dechiron/popular-articles',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tanguy-dechiron/popular-articles' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'grav-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'dd2b29dea11c7251867110bd4ad99de5e185b5ae',
+            'dev_requirement' => false,
+        ),
+    ),
+);
